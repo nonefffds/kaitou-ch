cc9475548a56e7f0f23e43047793b10efaf6aee8
FDS <fds@brunhild.cn>
2023-10-31 09:52:48 +0800

    Update lambda-serverless.py

--- lambda-serverless.py
+++ lambda-serverless.py
@@ -1,8 +1,10 @@
+import json
 import boto3
 import re
-import json
 
+s3 = boto3.client('s3')
 s3_client = boto3.client('s3')
+
 object_arn = 'arn:aws:s3:::phantom-vote-data/vote-count.json'  # Replace with your S3 ARN or S3 URI
 
 
@@ -16,69 +18,56 @@ def get_bucket_and_key_from_arn(arn):
         raise ValueError('Invalid S3 ARN or URI')
 
 
-bucket_name, object_key = get_bucket_and_key_from_arn(object_arn)
-
-
-def get_vote_count_from_s3():
-    try:
-        response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
-        vote_count_json = response['Body'].read().decode('utf-8')
-        vote_count_data = json.loads(vote_count_json)
-        if isinstance(vote_count_data, int):
-            return vote_count_data
-        else:
-            return vote_count_data.get('vote_count', 0)
-    except (json.JSONDecodeError, KeyError, ValueError):
-        return 0
-
-def update_vote_count_in_s3(vote_count):
-    try:
-        response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
-        vote_count_json = response['Body'].read().decode('utf-8')
-        vote_count_data = json.loads(vote_count_json)
-        vote_count_data['vote_count'] = vote_count
-        updated_vote_count_json = json.dumps(vote_count_data)
-        s3_client.put_object(Bucket=bucket_name, Key=object_key, Body=updated_vote_count_json)
-    except (json.JSONDecodeError, KeyError, ValueError):
-        pass
+bucket_name, file_name = get_bucket_and_key_from_arn(object_arn)
 
 def lambda_handler(event, context):
-    if event['httpMethod'] == 'GET':
-        vote_count = get_vote_count_from_s3()
-        return {
-            'statusCode': 200,
-            'body': {
-                'vote_count': vote_count
-            }
-        }
-
     if event['httpMethod'] == 'POST':
-        try:
-            vote = event['body']['option']
-        except (KeyError, TypeError):
-            return {
-                'statusCode': 400,
-                'body': 'Invalid request format. Vote must be in the format: {"option": "Yes"} or {"option": "No"}'
-            }
+        if isinstance(event['body'], dict):
+            body = event['body']
+        else:
+            body = json.loads(event['body'])
+        option = body['option']
+        #body = json.loads(event['body'])
+        if 'option' not in body or body['option'] not in ['Yes', 'No']:
+            return {'statusCode': 400, 'body': 'Bad Request'}
+        else:
+            vote_count = body['option']
+            update_vote_count(option)
+            return {'statusCode': 200, 'body': 'Vote Count Updated'}
+    elif event['httpMethod'] == 'GET':
+        vote_count = get_vote_count()
+        return {'statusCode': 200, 'body': json.dumps(vote_count)}
+    else:
+        return {'statusCode': 405, 'body': 'Method Not Allowed'}
+
+def get_vote_count():
+    try:
+        response = s3_client.get_object(Bucket=bucket_name, Key=file_name)
+        vote_count_json = response['Body'].read().decode('utf-8')
+        vote_count_data = json.loads(vote_count_json)
+        if 'Yes' in vote_count_data and 'No' in vote_count_data:
+            return {'statusCode': 200, 'body': {'Yes': vote_count_data['Yes'], 'No': vote_count_data['No']}}
+        else:
+            return {'statusCode': 500, 'body': 'Error: Yes and No keys not found in vote data'}
+    except (s3_client.exceptions.NoSuchKey, json.JSONDecodeError, KeyError, ValueError) as e:
+        return {'statusCode': 500, 'body': 'Error getting the vote count'}
         
-        if vote != "Yes" and vote != "No":
-            return {
-                'statusCode': 400,
-                'body': 'Invalid vote. Only "Yes" or "No" options are allowed.'
-            }
-
-        vote_count = get_vote_count_from_s3()
-        vote_count += 1
-        update_vote_count_in_s3(vote_count)
-
-        return {
-            'statusCode': 200,
-            'body': {
-                'vote_count': vote_count
-            }
-        }
-
-    return {
-        'statusCode': 405,
-        'body': 'Method not allowed'
-    }
+def update_vote_count(vote_count):
+    try:
+        response = s3_client.get_object(Bucket=bucket_name, Key=file_name)
+        vote_count_json = response['Body'].read().decode('utf-8')
+        vote_count_data = json.loads(vote_count_json)
+        if 'Yes' in vote_count_data:
+            vote_count_data['Yes'] = vote_count_data.get('Yes', 0) + 1
+            updated_vote_count_json = json.dumps(vote_count_data)
+            s3_client.put_object(Bucket=bucket_name, Key=file_name, Body=updated_vote_count_json)
+            return True
+        elif 'No' in vote_count_data:
+            vote_count_data['No'] = vote_count_data.get('No', 0) + 1
+            updated_vote_count_json = json.dumps(vote_count_data)
+            s3_client.put_object(Bucket=bucket_name, Key=file_name, Body=updated_vote_count_json)
+            return True
+        else:
+            return False
+    except (json.JSONDecodeError, KeyError, ValueError):
+        return False
